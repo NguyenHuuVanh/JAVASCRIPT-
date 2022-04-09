dc50464ca7d8d142bfaecb421861e2951c689f8f
NguyenHuuVanh <vietbui673@gmail.com>
2022-04-09 17:02:30 +0700

    Update file

---
+++ Day_1/main.js
@@ -0,0 +1,5 @@
+alert('Hi javascript basic')
+document.write('Hello Việt Anh')
+window.alert('Xin chào các bạn')
+console.log('Hô le mina-san')
+document.getElementById('demo2').innerHTML='Hello bạn nhỏ'
